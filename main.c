083e4e4bbd5e7c1d8b2542c04b4ea11b8a251e58
Atay36 <48059538+Atay36@users.noreply.github.com>
2019-04-12 11:02:22 +0900

    Update main.c

--- main.c
+++ main.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 
 
-void sub1(){
-
-};
+void sub1();
 
 void sub2();
+
+void sub3();
+
+void sub4();
 
 
 int main(){
@@ -13,6 +15,10 @@ int main(){
   sub1();
 
   sub2();
+	
+  sub3();
+	
+  sub4();
 
   return 0;
 
